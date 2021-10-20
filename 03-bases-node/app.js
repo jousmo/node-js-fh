74fba29d5df79d9162505060d5e57d49bedd0532
José Uscanga <jousmo@outlook.com>
2021-10-19 23:01:30 -0500

    feat: received arguments form cli

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,7 +2,8 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear()
 
-const base = 5
+const [, , arg3 = 'base=5'] = process.argv
+const [, base = 5] = arg3.split('=')
 
 crearArchivo(base)
   .then(console.log)
